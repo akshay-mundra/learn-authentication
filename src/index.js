5833d57947fd29283dfa7a6c5b09a2d6461a9c8e
Akshay Mundra <akshay@gkmit.co>
2024-10-24 16:03:25 +0530

    feat: connect mongodb

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-require('dotenv').config();
 const express = require('express');
+const dotenv = require('dotenv').config();
+const { connectDb } = require('./config/database.js');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+connectDb(); // database connection
 
 app.use(express.json());
 
 app.use('/health-check', (req, res) => {
-	res.send('hello ');
+	res.send('health check successfull ');
 });
 
 app.listen(PORT, () => {
