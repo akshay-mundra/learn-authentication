84ee329e20928b637d5f5fc17a6000524eabe4ba
Akshay Mundra <akshay@gkmit.co>
2024-10-25 11:16:01 +0530

    feat: add create user service

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,16 +1,26 @@
 const User = require('../models/user');
 const commonHelpers = require('../helpers/common.helper');
+const bcrypt = require('bcrypt');
 
 async function create(payload) {
 	const { username, password } = payload;
 
-	const usernameExists = await User.find({ username });
-
+	const usernameExists = await User.findOne({ username });
 	if (usernameExists) {
 		commonHelpers.throwCustomError('Username already exists', 409);
 	}
 
-	// const hashedPassword =
+	const hashedPassword = await bcrypt.hash(password, 10);
+	const userDetails = {
+		username,
+		password: hashedPassword,
+	};
+	const newUser = new User(userDetails);
+	await newUser.save();
+
+	return {
+		username: newUser.username,
+	};
 }
 
 module.exports = { create };
